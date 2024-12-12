b151208b41df6020881c26dc0411cd3991691685
mickeyescaleto <mickeyescaleto@gmail.com>
2024-12-12 16:38:22 +0300

    feat: add the ElysiaJS app

---
+++ apps/server/src/main.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
+import { swagger } from '@elysiajs/swagger';
+
+const app = new Elysia()
+  .use(cors())
+  .use(swagger())
+  .get('/', () => 'Hello Elysia')
+  .listen(3000, ({ url }) =>
+    console.log(`🦊 Application is running at ${url}`),
+  );
