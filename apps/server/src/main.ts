35cc8ccd1ff5ab4d0e9ac9144c4d19992eb389de
mickeyescaleto <mickeyescaleto@gmail.com>
2025-03-05 01:13:57 +0300

    feat(main.ts): add authentication and notifications routes to main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,11 +2,16 @@ import { Elysia } from 'elysia';
 import { cors } from '@elysiajs/cors';
 import { swagger } from '@elysiajs/swagger';
 
+import { config } from '@/config';
+import { authentication } from '@/routes/authentication';
+import { notifications } from '@/routes/notifications';
+
 const app = new Elysia()
   .use(cors())
   .use(swagger())
-  .get('/', () => 'Hello Elysia')
-  .listen(3001, ({ url }) =>
+  .use(authentication)
+  .use(notifications)
+  .listen(config.app.port, ({ url }) =>
     console.log(`🦊 Application is running at ${url}`),
   );
 
