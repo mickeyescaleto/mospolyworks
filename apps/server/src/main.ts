87d25764aee8dbf6b07558af7bc6f9dd5fa0e51e
mickeyescaleto <mickeyescaleto@gmail.com>
2025-03-05 01:14:57 +0300

    feat(main.ts): add cron to delete expired sessions

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,25 @@
 import { Elysia } from 'elysia';
 import { cors } from '@elysiajs/cors';
 import { swagger } from '@elysiajs/swagger';
+import { cron } from '@elysiajs/cron';
 
 import { config } from '@/config';
 import { authentication } from '@/routes/authentication';
 import { notifications } from '@/routes/notifications';
+import { SessionService } from '@/services/session';
 
 const app = new Elysia()
   .use(cors())
   .use(swagger())
+  .use(
+    cron({
+      name: 'verification',
+      pattern: '0 0 * * *',
+      run: async () => {
+        await new SessionService().deleteExpiredSessions();
+      },
+    }),
+  )
   .use(authentication)
   .use(notifications)
   .listen(config.app.port, ({ url }) =>
