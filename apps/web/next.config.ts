b8df02de0f13fd79f175869a8b7c8da27b635501
mickeyescaleto <mickeyescaleto@gmail.com>
2025-04-24 15:20:49 +0300

    chore(next.config.ts): update the next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,22 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   output: 'standalone',
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.slingacademy.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '6c303246-09c0-4a03-acf7-e714803867a6.selstorage.ru',
-      },
       {
         protocol: 'https',
         hostname: '5b34e788-79f4-4a49-86b3-eee20853bc80.selstorage.ru',
